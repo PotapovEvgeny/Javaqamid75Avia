801da6095dbadb8c4a897645f449209594ec76d5
Evgen7985 <Evgen7985@yandex.ru>
2023-11-05 17:20:04 +0500

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        
+
         int ticketPrice = 259847;
+
         int bonus = 20;
+
         int bonusMiles = 1;
 
         System.out.println(" Зачисленно " + ticketPrice * bonusMiles / bonus + " бонусных миль.");
